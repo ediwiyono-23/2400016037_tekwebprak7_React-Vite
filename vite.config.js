420eb3b53717314aec6a76d916e1630389e90f42
ediwiyono-23 <ediwiyono001@gmail.com>
2025-12-12 22:45:30 +0700

    setup github pages with docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/2400016037_tekwebprak7_React-Vite/',  
-})
+  base: "/2400016037_tekwebprak7_React-Vite/",
+  build: {
+    outDir: "docs",
+  },
+});
