8fec4115caab8addc063d9b5d0b45fe6b145e4c8
ediwiyono-23 <ediwiyono001@gmail.com>
2025-12-12 22:32:25 +0700

    fix github pages base

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/2400016037_tekwebprak7_React-Vite/',  
 })
